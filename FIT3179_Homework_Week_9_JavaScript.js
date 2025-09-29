7bfd56ef5b1ec6082c408e7355d300c018ee9da9
hamishroberts-159 <hrob0016@student.monash.edu>
2025-09-29 17:02:34 +1000

    Uploading JavaScript file

---
+++ FIT3179_Homework_Week_9_JavaScript.js
@@ -0,0 +1,7 @@
+var vg_1 = "https://raw.githubusercontent.com/hamishroberts-159/FIT3179-Visualisation2/refs/heads/main/FIT3179_Homework_Week_9.vg"; 
+vegaEmbed("#map", vg_1).then(function(result) {
+    
+    // Access the Vega view instance 
+    // (https://vega.github.io/vega/docs/api/view/) as result.view 
+    
+}).catch(console.error);
